d69376565df2c0e25d64a35a90f233fcf933c5ae
Abdullah5491 <123593816+Abdullah5491@users.noreply.github.com>
2023-11-13 15:40:43 +0500

    Update Bus.java

--- OOP Project/src/Bus.java
+++ OOP Project/src/Bus.java
@@ -2,8 +2,35 @@ public class Bus {
     private String name;
     private String Id;
 
-    private int Seats;
+    private int [] Seats;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public int[] getSeats() {
+        return Seats;
+    }
+
+    public void setSeats(int[] seats) {
+        Seats = seats;
+    }
+
+    public Bus(String name, String id, int[] seats) {
+        this.name = name;
+        Id = id;
+        Seats = seats;
+    }
 }
