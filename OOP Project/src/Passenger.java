b9d8bcbf109177e1ad0429d547f8fa7359f131ba
Abdul-Rehman Javed <127846553+Abdul-RehmanBCS@users.noreply.github.com>
2023-11-13 15:32:41 +0500

    update

--- OOP Project/src/Passenger.java
+++ OOP Project/src/Passenger.java
@@ -1,5 +1,11 @@
 public class Passenger {
     private String name;
     private int Id;
+
+    public Passenger(String name, int id) {
+        this.name = name;
+        Id = id;
+    }
     
+
 }
